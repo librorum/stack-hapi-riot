79ce253f2d439b24b5393bb495cd98d85878d856
Rodolfo do Nascimento Azevedo <rof20004@gmail.com>
2016-11-09 00:34:43 -0400

    Created index.js in each component

---
+++ components/security/index.js
@@ -0,0 +1,3 @@
+const Controller = require('./controller');
+
+exports.Controller = Controller;
--- routes/index.js
+++ routes/index.js
@@ -1,28 +1,24 @@
 'use strict';
 
-// Declared Views
-const UserView 			 	 	 = require('../components/user/view');
-const IndexView 		 		 = require('../components/index/view');
-const LoginView 		 		 = require('../components/login/view');
-const MapsView  		 		 = require('../components/maps/view');
-const TestView		 			 = require('../components/test_route/view');
-
-// Declared Controllers
-const SecurityController = require('../components/security/controller');
-const UserController  	 = require('../components/user/controller');
+const User 		 = require('../components/user');
+const Index 	 = require('../components/index');
+const Login 	 = require('../components/login');
+const Maps  	 = require('../components/maps');
+const Test		 = require('../components/test_route');
+const Security = require('../components/security');
 
 exports.endpoints = [
 
 	// Views
-	{method: 'GET',    	path: '/',													config: IndexView.index},
-	{method: 'GET',    	path: '/login',											config: LoginView.index},
-	{method: 'GET',    	path: '/test-route',								config: TestView.index},
-	{method: 'GET',    	path: '/user/add',									config: UserView.index},
-	{method: 'GET',    	path: '/maps',											config: MapsView.index},
+	{method: 'GET',    	path: '/',													config: Index.View.index},
+	{method: 'GET',    	path: '/login',											config: Login.View.index},
+	{method: 'GET',    	path: '/test-route',								config: Test.View.index},
+	{method: 'GET',    	path: '/user/add',									config: User.View.index},
+	{method: 'GET',    	path: '/maps',											config: Maps.View.index},
 
 	// Controllers
-	{method: 'POST',    path: '/register',									config: UserController.add},
-	{method: 'POST',    path: '/login',											config: SecurityController.login},
-	{method: 'POST',    path: '/logout',										config: SecurityController.logout}
+	{method: 'POST',    path: '/register',									config: User.Controller.add},
+	{method: 'POST',    path: '/login',											config: Security.Controller.login},
+	{method: 'POST',    path: '/logout',										config: Security.Controller.logout}
 
 ];
